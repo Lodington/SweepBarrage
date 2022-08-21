d02166b7a66e7ff6f663df9a3fd617533cc1ec06
Tyler Souza <tyler6souza@hotmail.com>
2022-08-21 12:03:28 -0400

    ProjectInit

---
+++ SweepBarrage/AutoShotPlugin.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using BepInEx;
+using BepInEx.Configuration;
+using EntityStates;
+using R2API;
+using RoR2;
+using RoR2.Skills;
+using UnityEngine;
+/*
+using R2API;
+using R2API.Utils;
+*/
+namespace AutoShot
+{
+	[BepInDependency("com.ThinkInvisible.ClassicItems", BepInDependency.DependencyFlags.SoftDependency)]
+	[BepInPlugin(GUID, MODNAME, VERSION)]
+    public sealed class AutoShotPlugin : BaseUnityPlugin
+    {
+        public const string
+            MODNAME = "AutoShot",
+            AUTHOR = "lodington",
+            GUID = "com." + AUTHOR + "." + MODNAME,
+            VERSION = "1.0.2";
+
+		public static ConfigEntry<float> damageCoefficient { get; set; }
+		public static ConfigEntry<int> minimumFireCount { get; set; }
+		public static ConfigEntry<float> procCoefficient { get; set; }
+		public static ConfigEntry<float> stunEnabled { get; set; }
+
+		SkillDef AUTO_SHOT;
+		SkillDef AUTO_SHOT_SCEPTER;
+		static GameObject CommandoBody = Resources.Load<GameObject>("Prefabs/CharacterBodies/CommandoBody");
+		public static AssetBundle assets;
+		SkillLocator skillLocator = CommandoBody.GetComponent<SkillLocator>();
+
+
+		public static AssetBundle _assets;
+
+		private void Awake() //Called when loaded by BepInEx.
+        {
+			var path = System.IO.Path.GetDirectoryName(Info.Location);
+			_assets = AssetBundle.LoadFromFile(System.IO.Path.Combine(path, "aeroltbundle"));
+
+			damageCoefficient = Config.Bind<float>("Damage Coefficient", "Damage Coefficient", 0.9f, "This Will set Damage Coefficient of Sweeping Barrage Skill Default Value is 0.9f");
+			minimumFireCount = Config.Bind<int>("minimum Fire Count", "minimum Fire Count", 10, "The minimum amount of times Sweeping barrage will shoot at targets less than X amount of enemys in range.");
+			procCoefficient = Config.Bind<float>("Proc CoEfficient", "Proc CoEfficient", 1.0f, "This Will set proc Coefficient of Sweeping Barrage Skill Default Value is 1.0f");
+
+			setupAutoShot();
+
+			if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.ThinkInvisible.ClassicItems"))
+			{
+				ScepterSkillSetup();
+				ScepterSetup();
+			}
+		}
+	
+		private void setupAutoShot()
+        {
+			AUTO_SHOT = ScriptableObject.CreateInstance<SkillDef>();
+			AUTO_SHOT.activationState = new SerializableEntityStateType(typeof(AutoShot));
+			AUTO_SHOT.skillNameToken = "Auto Shot";
+			AUTO_SHOT.skillName = "C_AUTO_SHOT";
+			AUTO_SHOT.skillDescriptionToken = "Fires a minimum of "+ minimumFireCount.Value + " <style=cIsUtility>auto-targeting</style> shots. Fire more shots depending on how many enemies are in your sights.";
+			AUTO_SHOT.activationStateMachineName = "Weapon";
+			AUTO_SHOT.baseMaxStock = 1;
+			AUTO_SHOT.baseRechargeInterval = 12f;
+			AUTO_SHOT.beginSkillCooldownOnSkillEnd = false;
+			AUTO_SHOT.canceledFromSprinting = false;
+			AUTO_SHOT.fullRestockOnAssign = true;
+			AUTO_SHOT.interruptPriority = InterruptPriority.Skill;
+			AUTO_SHOT.isCombatSkill = true;
+			AUTO_SHOT.mustKeyPress = false;
+			AUTO_SHOT.rechargeStock = 1;
+			AUTO_SHOT.requiredStock = 1;
+			AUTO_SHOT.cancelSprintingOnActivation = true; 
+			AUTO_SHOT.stockToConsume = 1;
+			AUTO_SHOT.icon = Resources.Load<Sprite>("@commandoskills:Assets/SWEEPING_BARRAGE.png");
+
+			LoadoutAPI.AddSkillDef(AUTO_SHOT);
+			SkillFamily skillFamily = skillLocator.special.skillFamily;
+			Array.Resize<SkillFamily.Variant>(ref skillFamily.variants, skillFamily.variants.Length + 1);
+
+			skillFamily.variants[skillFamily.variants.Length - 1] = new SkillFamily.Variant
+			{
+				skillDef = AUTO_SHOT,
+				unlockableName = "",
+				viewableNode = new ViewablesCatalog.Node(AUTO_SHOT.skillNameToken, false, null)
+			};
+
+		}
+
+		private void ScepterSkillSetup()
+        {
+
+			AUTO_SHOT_SCEPTER = ScriptableObject.CreateInstance<SkillDef>();
+			AUTO_SHOT_SCEPTER.activationState = new SerializableEntityStateType(typeof(BulletHell));
+			AUTO_SHOT_SCEPTER.skillNameToken = "Bullet Hell";
+			AUTO_SHOT_SCEPTER.skillName = "C_AUTO_SHOT_SCEPTER";
+			AUTO_SHOT_SCEPTER.skillDescriptionToken = "Fires a minimum of 25 <style=cIsUtility>auto-targeting</style> shots. Fires at everything on your screen.";
+			AUTO_SHOT_SCEPTER.activationStateMachineName = "Weapon";
+			AUTO_SHOT_SCEPTER.baseMaxStock = 1;
+			AUTO_SHOT_SCEPTER.baseRechargeInterval = 6f;
+			AUTO_SHOT_SCEPTER.beginSkillCooldownOnSkillEnd = false;
+			AUTO_SHOT_SCEPTER.canceledFromSprinting = false;
+			AUTO_SHOT_SCEPTER.fullRestockOnAssign = true;
+			AUTO_SHOT_SCEPTER.interruptPriority = InterruptPriority.Skill;
+			AUTO_SHOT_SCEPTER.isCombatSkill = true;
+			AUTO_SHOT_SCEPTER.mustKeyPress = false;
+			AUTO_SHOT.cancelSprintingOnActivation = false; 
+			AUTO_SHOT_SCEPTER.rechargeStock = 1;
+			AUTO_SHOT_SCEPTER.requiredStock = 1;
+			//AUTO_SHOT_SCEPTER.shootDelay = 1f;
+			AUTO_SHOT_SCEPTER.stockToConsume = 1;
+			AUTO_SHOT_SCEPTER.icon = Resources.Load<Sprite>("@commandoskills:Assets/SWEEPING_BARRAGE_SCEPTER.png");
+
+			LoadoutAPI.AddSkillDef(AUTO_SHOT_SCEPTER);
+		}
+
+		[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+		private void ScepterSetup()
+		{
+			bool test = ClassicItems.Scepter_V2.instance.RegisterScepterSkill(AUTO_SHOT_SCEPTER, "CommandoBody", SkillSlot.Special, 2);
+			bool ThinkInvisible.ClassicItems.Scepter.instance.RegisterScepterSkill(AUTO_SHOT_SCEPTER, "CommandoBody",
+				SkillSlot.Special, 2);
+		}
+		
+	}
+}
---
+++ SweepBarrage/BulletHell.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using EntityStates;
+using RoR2;
+
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace AutoShot
+{
+    class BulletHell : BaseState
+    {
+        public float baseDuration = 1.3f;
+        private float duration;
+        private float fireTimer;
+        public float timeBetweenBullets;
+        private float firingDuration;
+        public static float fieldOfView = float.MaxValue;
+        public float maxDistance = float.MaxValue;
+
+        public GameObject effectPrefab = Resources.Load<GameObject>("prefabs/effects/impacteffects/Hitspark");
+        public GameObject hitEffectPrefab = Resources.Load<GameObject>("prefabs/effects/impacteffects/critspark");
+        public GameObject tracerEffectPrefab = Resources.Load<GameObject>("prefabs/effects/tracers/tracerbanditshotgun");
+
+        private List<HurtBox> targetHurtboxes = new List<HurtBox>();
+
+        public static int minimumFireCount = 25;
+        public int totalBulletsToFire;
+        public int totalBulletsFired;
+        private int targetHurtboxIndex;
+        public float damage = 1.3f;
+
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            this.duration = this.baseDuration / base.attackSpeedStat;
+            this.firingDuration = this.baseDuration / this.attackSpeedStat;
+            Ray aimRay = base.GetAimRay();
+            base.characterBody.SetAimTimer(3f);
+            base.PlayAnimation("Gesture, Additive", "FireSweepBarrage", "FireSweepBarrage.playbackRate", this.baseDuration * 1.1f);
+            base.PlayAnimation("Gesture, Override", "FireSweepBarrage", "FireSweepBarrage.playbackRate", this.baseDuration * 1.1f);
+            var bullseyeSearch = new RoR2.BullseyeSearch();
+            bullseyeSearch.teamMaskFilter = TeamMask.GetEnemyTeams(base.GetTeam());
+            bullseyeSearch.filterByLoS = true;
+            bullseyeSearch.searchOrigin = aimRay.origin;
+            bullseyeSearch.searchDirection = aimRay.direction;
+            bullseyeSearch.sortMode = RoR2.BullseyeSearch.SortMode.DistanceAndAngle;
+            bullseyeSearch.maxDistanceFilter = maxDistance;
+            bullseyeSearch.maxAngleFilter = AutoShot.fieldOfView * 0.5f;
+            bullseyeSearch.RefreshCandidates();
+            //var hurtBox = bullseyeSearch.GetResults().FirstOrDefault();
+            this.targetHurtboxes = bullseyeSearch.GetResults().Where(new Func<HurtBox, bool>(Util.IsValid)).Distinct(default(HurtBox.EntityEqualityComparer)).ToList<HurtBox>();
+            this.totalBulletsToFire = Mathf.Max(this.targetHurtboxes.Count, minimumFireCount);
+            this.timeBetweenBullets = (this.firingDuration / (float)this.totalBulletsToFire);
+
+        }
+        private void Fire()
+        {
+            if (isListEmpty(this.targetHurtboxes))
+            {
+                return;
+            }
+            if (this.totalBulletsFired < this.totalBulletsToFire)
+            {
+                var localUser = RoR2.LocalUserManager.GetFirstLocalUser();
+                var controller = localUser.cachedMasterController;
+                if (!controller)
+                    return;
+                var body = controller.master.GetBody();
+                if (!body)
+                    return;
+                var inputBank = body.GetComponent<RoR2.InputBankTest>();
+                var aimRay = new Ray(inputBank.aimOrigin, inputBank.aimDirection);
+
+                if (this.targetHurtboxIndex >= this.targetHurtboxes.Count)
+                {
+                    this.targetHurtboxIndex = 0;
+                }
+                HurtBox hurtBox = this.targetHurtboxes[this.targetHurtboxIndex];
+                if (this.targetHurtboxes.Count > 0)
+                {
+                    if (hurtBox)
+                    {
+                        this.targetHurtboxIndex++;
+                        Vector3 direction = hurtBox.transform.position - aimRay.origin;
+                        //inputBank.aimDirection = direction;
+                        HealthComponent healthComponent = hurtBox.healthComponent;
+                        if (healthComponent)
+                        {
+                            if (base.isAuthority)
+                            {
+                                new BulletAttack
+                                {
+                                    owner = base.gameObject,
+                                    weapon = base.gameObject,
+                                    origin = aimRay.origin,
+                                    muzzleName = "MuzzleRight",
+                                    aimVector = direction,
+                                    minSpread = 0f,
+                                    maxSpread = base.characterBody.spreadBloomAngle,
+                                    bulletCount = 1U,
+                                    procCoefficient = AutoShotPlugin.procCoefficient.Value,
+                                    damage = base.characterBody.damage * damage,
+                                    force = 3,
+                                    falloffModel = BulletAttack.FalloffModel.DefaultBullet,
+                                    tracerEffectPrefab = this.tracerEffectPrefab,
+                                    hitEffectPrefab = this.hitEffectPrefab,
+                                    isCrit = base.RollCrit(),
+                                    HitEffectNormal = false,
+                                    damageType = DamageType.IgniteOnHit | DamageType.Stun1s,
+                                    stopperMask = LayerIndex.world.mask,
+                                    smartCollision = true,
+                                    maxDistance = maxDistance
+                                }.Fire();
+                            }
+                        }
+                    }
+                }
+                //Debug.Log("Bullet " + totalBulletsFired);
+                this.totalBulletsFired++;
+            }
+
+        }
+        public override void OnExit()
+        {
+            base.OnExit();
+        }
+        public override void FixedUpdate()
+        {
+            base.FixedUpdate();
+            this.fireTimer -= Time.fixedDeltaTime;
+            if (this.fireTimer <= 0f)
+            {
+                this.Fire();
+                this.fireTimer += this.timeBetweenBullets;
+            }
+            if (base.fixedAge >= this.duration && base.isAuthority)
+            {
+                this.outer.SetNextStateToMain();
+                return;
+            }
+        }
+        public bool isListEmpty(List<HurtBox> list)
+        {
+            return !list.Any();
+        }
+        public override InterruptPriority GetMinimumInterruptPriority()
+        {
+            return InterruptPriority.Death;
+        }
+    }
+}
+
